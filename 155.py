203dcced24ca8f89f1a1401da7064c88d3856659
dustineilers <eilersd23@gmail.com>
2024-09-20 23:03:29 -0400

    Stack problems

---
+++ 155.py
@@ -0,0 +1,23 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.min_stack[-1] if self.min_stack else val)
+        self.min_stack.append(val)
+
+    def pop(self) -> None:
+        self.min_stack.pop()
+        self.stack.pop()
+
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
